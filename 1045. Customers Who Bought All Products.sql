0f2596f19b2be42f9240b760b2e94507bbb9a6c5
Amarnath M <36194961+amarnathz@users.noreply.github.com>
2023-11-20 17:38:25 +0530

    Update 1045. Customers Who Bought All Products.sql

--- 1045. Customers Who Bought All Products.sql
+++ 1045. Customers Who Bought All Products.sql
@@ -1,7 +1,4 @@
-Select  C.customer_id 
-from Customer C
-left join Product P
-on  C.product_key = P.product_key  
-group by C.customer_id
-having  (select count(*) from Product ) =  count( distinct C.product_key )
-order by C.customer_id
+Select  customer_id 
+from Customer 
+group by customer_id
+having  (select count(*) from Product ) =  count( distinct product_key )
